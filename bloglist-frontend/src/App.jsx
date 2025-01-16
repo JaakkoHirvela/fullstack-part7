5300a282fcf2ae7f638e217398ca54a3d6a7cade
Jaakko Hirvelä <jaakko.hirvela@tuni.fi>
2025-01-16 23:15:44 +0200

    7.14: users view

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,45 +1,20 @@
-import { useState, useEffect, useRef, useContext, useMemo } from "react";
+import { useState, useEffect, useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import Blog from "./components/Blog";
-import blogService from "./services/blogs";
 import loginService from "./services/login";
-import BlogForm from "./components/BlogForm";
 import NotificationBar from "./components/NotificationBar";
 import LoginForm from "./components/LoginForm";
-import Togglable from "./components/Togglable";
 import { NotificationType, setNotification, useNotificationDispatch } from "./utils/notificationUtils";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
 import UserContext, { CLEAR_USER, SET_USER } from "./components/UserContext";
-import { useDeleteBlogMutation, useLikeBlogMutation, useNewBlogMutation } from "./services/mutations";
+import Blogs from "./components/Blogs";
+import Users from "./components/Users";
 
 const App = () => {
-  const queryClient = useQueryClient();
-  const notificationDispatch = useNotificationDispatch();
   const [user, userDispatch] = useContext(UserContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const blogFormRef = useRef();
-
-  const result = useQuery({
-    queryKey: ["blogs"],
-    queryFn: () => {
-      return blogService.getAll();
-    },
-    retry: 1,
-  });
-
-  const { data: blogs, error, isLoading, isError } = result;
-
-  useEffect(() => {
-    if (isLoading) {
-      setNotification(notificationDispatch, "Loading blogs...", NotificationType.INFO);
-    } else if (isError) {
-      setNotification(notificationDispatch, "Failed to load blogs: " + error.message, NotificationType.ERROR);
-    } else if (blogs) {
-      setNotification(notificationDispatch, "Blogs loaded", NotificationType.SUCCESS);
-    }
-  }, [isLoading, isError]);
+  const notificationDispatch = useNotificationDispatch();
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedUser");
@@ -70,12 +45,6 @@ const App = () => {
     userDispatch({ type: CLEAR_USER });
   };
 
-  const afterNewBlog = () => blogFormRef.current.toggleVisibility();
-
-  const newBlogMutation = useNewBlogMutation(queryClient, user, notificationDispatch, afterNewBlog);
-  const likeBlogMutation = useLikeBlogMutation(queryClient, user, notificationDispatch);
-  const deleteBlogMutation = useDeleteBlogMutation(queryClient, user, notificationDispatch);
-
   if (user === null) {
     return (
       <div>
@@ -89,31 +58,22 @@ const App = () => {
           setPassword={setPassword}
         />
       </div>
-    ); // TODO: Clean up the login form.
+    );
   }
   return (
     <div>
       <h2>blogs</h2>
       <NotificationBar />
-      <div style={{ paddingBottom: "10px" }}>
-        Logged in as {user.name}
-        <button onClick={handleLogout}>logout</button>
-      </div>
-      <Togglable buttonLabel="new blog" ref={blogFormRef}>
-        <BlogForm createBlog={newBlogMutation.mutate} />
-      </Togglable>
-      {blogs &&
-        blogs
-          .sort((l, r) => r.likes - l.likes) // Sort in descending order.
-          .map((blog) => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              handleLike={likeBlogMutation.mutate}
-              handleDelete={deleteBlogMutation.mutate}
-              user={user}
-            />
-          ))}
+      <div style={{ paddingBottom: "10px" }}>Logged in as {user.name}</div>
+      <button onClick={handleLogout}>
+        logout
+      </button>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Blogs user={user} />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
---
+++ bloglist-frontend/src/components/Blogs.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from "react";
+import { useDeleteBlogMutation, useLikeBlogMutation, useNewBlogMutation } from "../services/blogsMutations";
+import { NotificationType, setNotification, useNotificationDispatch } from "../utils/notificationUtils";
+import BlogForm from "./BlogForm";
+import Togglable from "./Togglable";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import blogService from "../services/blogs";
+import Blog from "./Blog";
+
+const Blogs = ({ user }) => {
+  const blogFormRef = useRef();
+  const notificationDispatch = useNotificationDispatch();
+  const queryClient = useQueryClient();
+  const result = useQuery({
+    queryKey: ["blogs"],
+    queryFn: () => {
+      return blogService.getAll();
+    },
+    retry: 1,
+  });
+
+  const { data: blogs, error, isLoading, isError } = result;
+
+  useEffect(() => {
+    if (isLoading) {
+      setNotification(notificationDispatch, "Loading blogs...", NotificationType.INFO);
+    } else if (isError) {
+      setNotification(notificationDispatch, "Failed to load blogs: " + error.message, NotificationType.ERROR);
+    } else if (blogs) {
+      setNotification(notificationDispatch, "Blogs loaded", NotificationType.SUCCESS);
+    }
+  }, [isLoading, isError]);
+
+  const afterNewBlog = () => blogFormRef.current.toggleVisibility();
+  const newBlogMutation = useNewBlogMutation(queryClient, user, notificationDispatch, afterNewBlog);
+  const likeBlogMutation = useLikeBlogMutation(queryClient, user, notificationDispatch);
+  const deleteBlogMutation = useDeleteBlogMutation(queryClient, user, notificationDispatch);
+
+  return (
+    <div>
+      <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <BlogForm createBlog={newBlogMutation.mutate} />
+      </Togglable>
+      {blogs &&
+        blogs
+          .sort((l, r) => r.likes - l.likes) // Sort in descending order.
+          .map((blog) => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              handleLike={likeBlogMutation.mutate}
+              handleDelete={deleteBlogMutation.mutate}
+              user={user}
+            />
+          ))}
+    </div>
+  );
+};
+
+export default Blogs;
